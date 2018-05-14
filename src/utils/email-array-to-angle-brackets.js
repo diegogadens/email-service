56dd1ad8cda862b32195c6ab4a1963a593fd6ae1
Diego Gadens dos Santos <diegogadens@gmail.com>
2018-05-14 21:27:39 +1000

    adding util to convert an array of emails to angle bracket syntax

---
+++ src/utils/email-array-to-angle-brackets.js
@@ -0,0 +1,13 @@
+module.exports = (array) => {
+  if(array.length > 0){
+    let emails = '';
+    for(let i=0; i<(array.length - 1); i++)
+      emails += `<${array[i]}>, `;
+
+    emails += `<${array[(array.length - 1)]}>`;
+
+    return emails;
+  }
+  else
+    return null
+}
---
+++ test/utils/email-array-to-angle-brackets.spec.js
@@ -0,0 +1,40 @@
+const emailsParser = require('../../src/utils/email-array-to-angle-brackets')
+const should       = require('should');
+
+describe('Utils', () => {
+  describe('Convert array of emails to angle brackets', () => {
+
+    it('it should convert an empty array to a null string', (done) => {
+      emailArray = [];
+      expected = null;
+
+      should(emailsParser(emailArray)).eql(expected);
+      return done();
+    });
+
+    it('it should convert an array with one email to the correct angle brackets syntax', (done) => {
+      emailArray = ['test@mail.com'];
+      expected = '<test@mail.com>';
+
+      should(emailsParser(emailArray)).eql(expected);
+      return done();
+    });
+
+    it('it should convert an array with 2 emails to the correct angle brackets syntax', (done) => {
+      emailArray = ['test@mail.com', 'test2@mail.com'];
+      expected = '<test@mail.com>, <test2@mail.com>';
+
+      should(emailsParser(emailArray)).eql(expected);
+      return done();
+    });
+
+    it('it should convert an array with 3 emails to the correct angle brackets syntax', (done) => {
+      emailArray = ['test@mail.com', 'test2@mail.com', 'test3@mail.com'];
+      expected = '<test@mail.com>, <test2@mail.com>, <test3@mail.com>';
+
+      should(emailsParser(emailArray)).eql(expected);
+      return done();
+    });
+
+  });
+});
