670b329d460b20746cb0414c5ffed1594ac486a7
Diego Gadens dos Santos <diegogadens@gmail.com>
2018-05-15 22:30:04 +1000

    updating the email controller

--- src/controllers/email-controller.js
+++ src/controllers/email-controller.js
@@ -5,28 +5,27 @@ log       = require('../log')
 exports.post = (req, res, next) => {
   const { to, cc, bcc, subject, message } = req.body;
 
-  // amazonSES.sendEmail(to, cc, bcc, subject, message, (err, code, response) => {
-  //   if(err){
-  //     log.error(`Error while sending email: ${err}`)
-  //     return next(err)
-  //   }
-  //   else{
-  //     res.send(code, response);
-  //     return next();
-  //   }
-  // })
-
-
-  mailGun.sendEmail(to, cc, bcc, subject, message, (err, code, response) => {
+  amazonSES.sendEmail(to, cc, bcc, subject, message, (err, response) => {
     if(err){
       log.error(`Error while sending email: ${err}`)
       return next(err)
     }
     else{
-      res.send(code, response);
+      res.send(response.code, response);
       return next();
     }
   })
+
+  // mailGun.sendEmail(to, cc, bcc, subject, message, (err, response) => {
+  //   if(err){
+  //     log.error(`Error while sending email: ${err}`)
+  //     return next(err)
+  //   }
+  //   else{
+  //     res.send(response.code, response);
+  //     return next();
+  //   }
+  // })
 
 
 
