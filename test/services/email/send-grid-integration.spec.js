ba4b63a1d28e4dbdce81e800549ca49de47de9cb
Diego Gadens <diego.gadens@tabcorp.com.au>
2018-05-16 16:53:53 +1000

    adding tests for Send Grid

---
+++ test/services/email/send-grid-integration.spec.js
@@ -0,0 +1,89 @@
+const sendGrid   = require('../../../src/services/email/send-grid');
+const config     = require('../../../config');
+const should     = require('should');
+const sinon      = require('sinon');
+const request    = require('request');
+
+describe('Integration', () => {
+
+  let sandbox = null;
+
+  describe('Email', () => {
+    describe('SendGrid', () => {
+
+      beforeEach(() => {
+        sandbox = sinon.createSandbox();
+        sandbox.spy(request, 'post');
+      });
+
+      afterEach(() => {
+        sandbox.restore();
+      })
+
+      it('should send an email successfully through SendGrid', (done) => {
+
+        const { url, apiKey, fromAddress } = config.emailProviders.sendGrid
+        const to = ['diegogadens@gmail.com']
+        const subject = 'Integration test'
+        const message = 'This email has been sent as part of an integration test with Send Grid.'
+
+        sendGrid.sendEmail(to, null, null, subject, message, (err, response) => {
+          should.not.exist(err)
+          sinon.assert.calledWith(request.post, {
+            url:`${url}`,
+            headers: {
+              Authorization: `Bearer ${apiKey}`,
+              'Content-Type': 'application/json'
+            },
+            json: {
+              personalizations:[{
+                to: [{ email: to[0]}],
+                subject: subject
+              }],
+              from: { email: fromAddress },
+              content: [{
+                type: "text/plain",
+                value: message
+              }]
+            }
+          });
+          response.code.should.eql(202);
+          done()
+        });
+      });
+
+      it('should receive an error response from SendGrid', (done) => {
+
+        const { url, apiKey, fromAddress } = config.emailProviders.sendGrid
+        const to = ['diegogadens@gmail.com']
+        const subject = 'Subject'
+        const message = 'The message'
+
+        sendGrid.sendEmail([], null, null, subject, message, (err, response) => {
+          should.not.exist(err)
+          sinon.assert.calledWith(request.post, {
+            url:`${url}`,
+            headers: {
+              Authorization: `Bearer ${apiKey}`,
+              'Content-Type': 'application/json'
+            },
+            json: {
+              personalizations:[{
+                to: [],
+                subject: subject
+              }],
+              from: { email: fromAddress },
+              content: [{
+                type: "text/plain",
+                value: message
+              }]
+            }
+          });
+
+          response.error.code.should.eql(400);
+          done()
+        });
+      });
+    });
+  });
+});
---
+++ test/services/email/send-grid.spec.js
@@ -0,0 +1,114 @@
+const sendGrid   = require('../../../src/services/email/send-grid');
+const config     = require('../../../config');
+const should     = require('should');
+const sinon      = require('sinon');
+const request    = require('request');
+
+describe('Services', () => {
+
+  let sandbox = null;
+
+  describe('Email', () => {
+    describe('SendGrid', () => {
+
+      beforeEach(() => {
+        sandbox = sinon.createSandbox();
+        sandbox.stub(request, 'post');
+      });
+
+      afterEach(() => {
+        sandbox.restore();
+      })
+
+      it('should send an email successfully through SendGrid', (done) => {
+        request.post.yields(null, {statusCode: 202}, 'SendGrid response');
+
+        const { url, apiKey, fromAddress } = config.emailProviders.sendGrid
+        const to = ['diegogadens@gmail.com']
+        const subject = 'Subject'
+        const message = 'The message'
+
+        sendGrid.sendEmail(to, null, null, subject, message, (err, response) => {
+          should.not.exist(err)
+          sinon.assert.calledWith(request.post, {
+            url:`${url}`,
+            headers: {
+              Authorization: `Bearer ${apiKey}`,
+              'Content-Type': 'application/json'
+            },
+            json: {
+              personalizations:[{
+                to: [{ email: to[0]}],
+                subject: subject
+              }],
+              from: { email: fromAddress },
+              content: [{
+                type: "text/plain",
+                value: message
+              }]
+            }
+          });
+          response.should.eql({
+            code: 202,
+            message: 'SendGrid response'
+          });
+          done()
+        });
+      });
+
+      it('should receive an error response from SendGrid', (done) => {
+        request.post.yields(null, {statusCode: 401}, 'SendGrid response');
+
+        const { url, apiKey, fromAddress } = config.emailProviders.sendGrid
+        const to = ['diegogadens@gmail.com']
+        const subject = 'Subject'
+        const message = 'The message'
+
+        sendGrid.sendEmail(to, null, null, subject, message, (err, response) => {
+          should.not.exist(err)
+          sinon.assert.calledWith(request.post, {
+            url:`${url}`,
+            headers: {
+              Authorization: `Bearer ${apiKey}`,
+              'Content-Type': 'application/json'
+            },
+            json: {
+              personalizations:[{
+                to: [{ email: to[0]}],
+                subject: subject
+              }],
+              from: { email: fromAddress },
+              content: [{
+                type: "text/plain",
+                value: message
+              }]
+            }
+          });
+
+          response.should.eql({
+            error:{
+              code:401,
+              message: 'SendGrid response'
+            }
+          });
+          done()
+        });
+      });
+
+      it('should receive an http error while trying to send a request to mailgun', (done) => {
+        request.post.yields('some http error');
+
+        const  { url, apiKey, fromAddress } = config.emailProviders.sendGrid
+        const to = ['diegogadens@gmail.com']
+        const subject = 'Subject'
+        const message = 'The message'
+
+        sendGrid.sendEmail(to, null, null, subject, message, (err, response) => {
+          should.exist(err)
+          err.should.eql('some http error');
+          done()
+        });
+      });
+    });
+  });
+});
